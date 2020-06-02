f9ed27d48090f1db17af08332be5013d2dd41223
Radek <wandzel.radoslaw@gmail.com>
2020-06-02 09:39:16 +0200

    created db models

--- project/server/models.py
+++ project/server/models.py
@@ -1,9 +1,9 @@
 # project/server/models.py
 
 
-import datetime
-
 from flask import current_app
+from enum import Enum
+from datetime import datetime
 
 from project.server import db, bcrypt
 
@@ -23,7 +23,7 @@ class User(db.Model):
         self.password = bcrypt.generate_password_hash(
             password, current_app.config.get("BCRYPT_LOG_ROUNDS")
         ).decode("utf-8")
-        self.registered_on = datetime.datetime.now()
+        self.registered_on = datetime.now()
         self.admin = admin
 
     def is_authenticated(self):
@@ -40,3 +40,75 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User {0}>".format(self.email)
+
+class Product(db.Model):
+
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    permalink = db.Column(db.String(255), nullable=False)
+    date_created_gmt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+
+    def __init__(self, name, permalink, description, price, weight):
+        self.name = name
+        self.permalink = permalink
+        self.description = description
+        self.price = price
+        self.weight = weight
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return "<Product {0}>".format(self.name)
+
+class OrderItem(db.Model):
+    __tablename__ = "order_items"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    quantity = db.Column(db.Integer, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+
+    product = db.relationship("Product")
+
+    def total(self):
+        return self.product.price * self.quantity
+
+class Order(db.Model):
+
+    __tablename__ = "orders"
+
+    class STATUS(Enum):
+        PENDING = 'pending'
+        PROCESSING = 'processing'
+        ON_HOLD = 'on-hold'
+        COMPLETED = 'completed'
+        CANCELLED = 'cancelled'
+        REFUNDED = 'refunded'
+        FAILED = 'failed'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    number = db.Column(db.String(255), nullable=False, unique=True)
+    status = db.Column(db.Enum(STATUS), nullable=False, default=STATUS.PENDING)
+    date_created_gmt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    modification_date_gmt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    order_items = db.relationship("OrderItem")
+
+    def get_id(self):
+        return self.id
+
+    def total(self):
+        return sum(oi.total() for oi in self.order_items)
+
+    def get_status(self):
+        return self.status;
+
+    def __repr__(self):
+        return "<Order {0}>".format(self.number)
